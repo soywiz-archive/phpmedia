93322a70b0313a91f323c25b3b5704bb222a3ace
soywiz <soywiz@00583b9e-2749-11de-9ca7-1beefeb68fa5>
2009-04-26 14:35:39 +0000

    - Minor improvements in Bitmap - Font::__get, Font::__set, utf8_decode, Font::metrics, Font::STYLE_*

--- trunk/php_media_font.c
+++ trunk/php_media_font.c
@@ -48,6 +48,70 @@ PHP_METHOD(Font, width)
 	FontCheckInit(); if (zend_parse_parameters(ZEND_NUM_ARGS(), TSRMLS_C, "|s", &text, &text_len) == FAILURE) RETURN_FALSE;
 }
 
+// Font::__get($key)
+PHP_METHOD_ARGS(Font, __get) ARG_INFO(key) ZEND_END_ARG_INFO()
+PHP_METHOD(Font, __get)
+{
+	char *key; int key_l;
+	THIS_FONT;
+	if (zend_parse_parameters(ZEND_NUM_ARGS(), TSRMLS_C, "s", &key, &key_l) == FAILURE) RETURN_FALSE;
+
+	if (strcmp(key, "fixed"    ) == 0) RETURN_BOOL(TTF_FontFaceIsFixedWidth(font->font));
+	if (strcmp(key, "style"    ) == 0) RETURN_LONG(TTF_GetFontStyle(font->font));
+	if (strcmp(key, "height"   ) == 0) RETURN_LONG(TTF_FontHeight(font->font));
+	if (strcmp(key, "ascent"   ) == 0) RETURN_LONG(TTF_FontAscent(font->font));
+	if (strcmp(key, "descent"  ) == 0) RETURN_LONG(TTF_FontDescent(font->font));
+	if (strcmp(key, "likeSkip" ) == 0) RETURN_LONG(TTF_FontLineSkip(font->font));
+	if (strcmp(key, "faceName" ) == 0) RETURN_STRING(TTF_FontFaceFamilyName(font->font), 1);
+	if (strcmp(key, "styleName") == 0) RETURN_STRING(TTF_FontFaceStyleName(font->font), 1);
+	
+	RETURN_FALSE;
+}
+
+// Font::__set($key, $value)
+PHP_METHOD_ARGS(Font, __set) ARG_INFO(key) ARG_INFO(value) ZEND_END_ARG_INFO()
+PHP_METHOD(Font, __set)
+{
+	char *key; int key_l; int v;
+	THIS_FONT;
+	if (zend_parse_parameters(ZEND_NUM_ARGS(), TSRMLS_C, "sl", &key, &key_l, &v) == FAILURE) RETURN_FALSE;
+
+	if (strcmp(key, "style"   ) == 0) {
+		TTF_SetFontStyle(font->font, v);
+		RETURN_LONG(TTF_GetFontStyle(font->font));
+	}
+}
+
+// Font::metrics($char)
+PHP_METHOD_ARGS(Font, metrics) ARG_INFO(key) ZEND_END_ARG_INFO()
+PHP_METHOD(Font, metrics)
+{
+	char *key; int key_l;
+	zval *array;
+	THIS_FONT;
+	if (zend_parse_parameters(ZEND_NUM_ARGS(), TSRMLS_C, "s", &key, &key_l) == FAILURE) RETURN_FALSE;
+	{
+		int minx, miny;
+		int maxx, maxy;
+		int advance;
+		Uint16 ch = (Uint16)utf8_decode(key, NULL);
+		TTF_GlyphMetrics(font->font, ch, &minx, &maxx, &miny, &maxy, &advance);
+		
+		MAKE_STD_ZVAL(array);
+		array_init(array);
+
+		add_assoc_long(array, "minx", minx);
+		add_assoc_long(array, "miny", miny);
+		add_assoc_long(array, "maxx", maxx);
+		add_assoc_long(array, "maxy", maxy);
+		add_assoc_long(array, "width", maxx - minx);
+		add_assoc_long(array, "height", maxy - miny);
+		add_assoc_long(array, "advance", advance);
+		
+		RETURN_ZVAL(array, 0, 1);
+	}
+}
+
 // Font::height($text = '')
 PHP_METHOD_ARGS(Font, height) ARG_INFO(text) ZEND_END_ARG_INFO()
 PHP_METHOD(Font, height)
